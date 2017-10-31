acdd2835d104d9303698c4cda23a5f75339167c1
bluufish <simonmchan92@gmail.com>
2017-10-31 15:06:39 -0400

    F/add cart test specs

--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -1,7 +1,10 @@
 
-import {expect} from 'chai'
+import { expect } from 'chai'
+import { addItem } from './cart'
 import configureMockStore from 'redux-mock-store'
-import {addItem} from './cart'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import cartReducer from './cart'
+
 
 const middlewares = []
 const mockStore = configureMockStore(middlewares)
@@ -12,17 +15,35 @@ describe('Cart Reducer', () => {
 
     beforeEach(() => {
         store = mockStore(initialState)
-      })
+    })
 
-      afterEach(() => {
+    afterEach(() => {
         store.clearActions()
-      })
+    })
 
-      it('adds an item to the cart', () => {
-          const item = {id: 1, quantity: 1, name: Apple}
-          const action = addItem(item)
-          store.dispatch(action)
+    describe('Sends Add Item action to Store', () => {
+        it('dispatches the ADD ITEM action', () => {
+            const item = { id: 1, quantity: 1, name: 'Apple' }
+            const action = addItem(item)
+            store.dispatch(action)
 
-          expect(store.getState().length).to.be.equal.equal(1)
-      })
+            const actions = store.getActions()
+            expect(actions[0].type).to.be.equal('ADD_ITEM')
+        })
+    })
+
+    describe('Add item', () => {
+        const store = createStore(cartReducer)
+        it('adds an item to the cart', () => {
+            const item = { id: 1, quantity: 1, name: 'Banana' }
+            const action = addItem(item)
+            store.dispatch(action)
+            expect(store.getState().length).to.be.equal(1)
+        })
+    
+
+    // describe('Remove Item', () => {
+    // })
+
+    })
 })
