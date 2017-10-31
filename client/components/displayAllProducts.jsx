b1c5f05d015d288c8fc603137382eb7fd4a7ab68
Swieckowski <a.swieckowski@gmail.com>
2017-10-31 17:20:11 -0400

    Got back what was initially lost in Arthur's git mistake

--- client/components/displayAllProducts.jsx
+++ client/components/displayAllProducts.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {displayProduct as DisplayProduct} from './displayProduct';
+import {displayProduct as DisplayProduct} from './displayProduct.jsx';
 
 //This is a component to display all products passed down to it.
 export const displayAllProducts = (props) => {
@@ -7,7 +7,7 @@ export const displayAllProducts = (props) => {
   return (
     <div className="displayAllProducts">
       {
-        ingredients.map((ingredient, i)=><DisplayProduct key={i} ingredient={ingredient}/>
+        ingredients.map((ingredient, i)=><DisplayProduct key={i} ingredient={ingredient}/>)
       }
     </div>
   )
