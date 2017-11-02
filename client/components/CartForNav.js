4699db2469feab9cbcc56cc57f285f520a0c36ea
bluufish <simonmchan92@gmail.com>
2017-11-02 14:26:03 -0400

    Fix: Cart Componenet didnt wait for items to load

--- client/components/CartForNav.js
+++ client/components/CartForNav.js
@@ -4,23 +4,24 @@ import { Link, withRouter } from 'react-router-dom';
 
 
 const CartComponent = (props) => {
-    const itemInformation = (itemOnCart) => (props.ingredients.find(ingredient => +ingredient.id === +itemOnCart.item_id))
 
+    const itemInformation = (itemOnCart) => (props.ingredients.find(ingredient => +ingredient.id === +itemOnCart.item_id))
     const totalItemsonCart = props.itemsOnCart.reduce((cartQuantity, currentItem) => {
         return cartQuantity + currentItem.quantity
     }, 0)
 
-    const totalCaloriesOnCart = props.itemsOnCart.reduce((cartCalories, currentItem) => {
-        return cartCalories + itemInformation(currentItem).calories * currentItem.quantity
-    }, 0)
+    if (props.ingredients.length && props.itemsOnCart.length) {
+        const totalCaloriesOnCart = props.itemsOnCart.reduce((cartCalories, currentItem) => {
+            return cartCalories + itemInformation(currentItem).calories * currentItem.quantity
+        }, 0)
 
-    return (
-        <div>
-        <Link to = '/cart'>
-            CART : {totalItemsonCart} items Calories : {totalCaloriesOnCart}
-        </Link>
-        </div>
-    )
+        return (
+            <div>
+                <Link to='/cart'>CART</Link>: {totalItemsonCart} items Calories: {totalCaloriesOnCart}
+            </div>
+        )
+    }
+    else return <div></div>
 }
 
 const mapState = (state) => ({
