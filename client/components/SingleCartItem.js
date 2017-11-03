de339f4309f72b3138f953e677c5058f6a730a66
bluufish <simonmchan92@gmail.com>
2017-11-03 12:53:32 -0400

    fix:update adds selected value to quantity

--- client/components/SingleCartItem.js
+++ client/components/SingleCartItem.js
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import _ from 'lodash'
 
 import Button from './button'
 
-const SingleCartItem = (props) => {
-    const { itemOnCart, productInfo, activeOrder } = props
-    if (!productInfo) return <li>Loading Item</li>
+class SingleCartItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedQuantity: 1
+        }
+    }
 
-    return (
-        <li>
-            <Link to={`/ingredients/${itemOnCart.item_id}`}>
-                {productInfo.name}
-            </Link>
-            
-            ID: {itemOnCart.item_id} Quantity: {itemOnCart.quantity}
+    render() {
+        const { itemOnCart, productInfo, activeOrder } = this.props
+        if (!productInfo) return <li>Loading Item</li>
 
-            {//check if its cart view or past order and disables button if its a past order
-                activeOrder && (<div>
-                    <Button buttonType={"REMOVE_ITEM"} buttonText={"DELETE"} item_id={itemOnCart.item_id} />
-                    <Button buttonType={"UPDATE_ITEM"} buttonText={"UPDATE"} item_id={itemOnCart.item_id} quantity={1} />
-                </div>)
+        //helper function
+        function listOfNumbers(n) {
+            const list = []
+            for (let i = 1; i <= n; i++) {
+                list.push(<option key={i}>{i}</option>)
             }
-        </li>
-    )
+            return list
+        }
+
+        return (
+            <li>
+                <Link to={`/ingredients/${itemOnCart.item_id}`}>
+                    {productInfo.name}
+                </Link>
+
+                ID: {itemOnCart.item_id} Quantity: {itemOnCart.quantity}
+
+                {//check if its cart view or past order and disables button if its a past order
+                    activeOrder && (<div>
+                        <Button
+                            buttonType={"REMOVE_ITEM"}
+                            buttonText={"DELETE"}
+                            item_id={itemOnCart.item_id} />
+                        <Button
+                            buttonType={"UPDATE_ITEM"}
+                            buttonText={"UPDATE"}
+                            item_id={itemOnCart.item_id}
+                            quantity={this.state.selectedQuantity} />
+
+                        <select
+                            value={this.state.selectedQuantity}
+                            onChange={(event) => { this.setState({ selectedQuantity: Number(event.target.value) }) }}>
+                            {listOfNumbers(10)}
+                        </select>
+
+                    </div>)
+                }
+            </li>
+        )
+    }
 }
 
 export default SingleCartItem
