ad7ed24b7214105193d21414e502a1e41224fa1d
daddyyun <shysam.yun@gmail.com>
2017-11-06 20:22:17 -0500

    Update orderIngredient.js

--- server/db/models/orderIngredient.js
+++ server/db/models/orderIngredient.js
@@ -2,10 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const OrderIngredient = db.define('orderIngredient', {
-    // id: {
-    //     type: Sequelize.INTEGER,
-    //     primaryKey: true
-    // },
     quantity: Sequelize.INTEGER
 })
 
