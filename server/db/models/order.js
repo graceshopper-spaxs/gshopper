032f61e530e78c02de21c8e7166512376cd55006
Sisi Qin <xixiqinms@gmail.com>
2017-11-07 15:17:42 -0500

    feat/able to send comfirmation and shipping email

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const User = require('./user')
+const {notificationEmail, shippingEmail} = require('../../email')
 
 const Order = db.define('order', {
     orderTime: {
@@ -18,5 +20,26 @@ const Order = db.define('order', {
     address: {
         type: Sequelize.STRING
     },
+    status: {
+        type: Sequelize.ENUM('created', 'processed', 'completed', 'cancelled'),
+        defaultValue: "created"
+    }
 })
 module.exports = Order;
+
+Order.hook('afterCreate', (order, option) => {
+    const orderId = order.dataValues.id    
+    return User.findById(order.dataValues.userId)
+        .then(result => result.dataValues)
+        .then(user => notificationEmail(user.firstName, user.email, orderId))
+})
+
+Order.hook('afterUpdate', (order, option) => {
+    if(order.dataValues.status === "completed"){
+        const orderId = order.dataValues.id
+        console.log(orderId)
+        return User.findById(order.dataValues.userId)
+        .then(result => result.dataValues)
+        .then(user => shippingEmail(user.firstName, user.email, orderId)) 
+    }
+})
