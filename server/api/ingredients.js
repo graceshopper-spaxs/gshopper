eb10ae139f3c90140dfeb1d88cd629848b3c94e1
Samuel Yun <shysam.yun@gmail.com>
2017-11-06 20:14:52 -0500

    finished checking product availability

--- server/api/ingredients.js
+++ server/api/ingredients.js
@@ -5,12 +5,7 @@ module.exports = router
 
 
 router.get('/', (req, res, next) => {
-    Ingredient.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id','name', 'calories', 'price', 'image', 'servingSize']
-    })
+    Ingredient.findAll()
       .then(ingredients => res.json(ingredients))
       .catch(next)
   })
