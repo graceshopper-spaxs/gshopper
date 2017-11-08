09e6cf8f8d3cc93d97870bf309ab72baf0b1d253
Philip Fahim <philip.louis49@gmail.com>
2017-11-08 08:51:39 -0500

    styling: used material-UI to add styling to buttons, lists and tables

--- client/components/displayAllProducts.js
+++ client/components/displayAllProducts.js
@@ -1,5 +1,21 @@
 import React from 'react'
-import { displayProduct as DisplayProduct } from './displayProduct'
+import { NavLink } from 'react-router-dom'
+import { GridList, GridTile } from 'material-ui/GridList'
+import IconButton from 'material-ui/IconButton'
+import AddButton from './button'
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
+};
 
 //This is a component to display all products passed down to it.
 export const displayAllProducts = (props) => {
@@ -14,16 +30,28 @@ export const displayAllProducts = (props) => {
 
   return (
     <div>
-      <div className="displayAllProducts">
-        {
-          ingredients.map((ingredient, i) => <DisplayProduct key={i} ingredient={ingredient} />)
-        }
-      </div>
-      <div>
-        {
-          !ingredients.length &&
-          <h5>Products not Found</h5>
-        }
+      <div style={styles.root}>
+        <GridList
+          cellHeight={180}
+          style={styles.gridList}>
+          {ingredients.map((ingredient, i) => (
+            <GridTile
+              key={i}
+              title={ingredient.name}
+              subtitle={<span>
+                calories: <b>{ingredient.calories}</b> <br />
+                price: $<b>{ingredient.price}</b> <br />
+                <b>{ingredient.servingSize}</b> grams per serving <br />
+              </span>}
+              actionIcon={<IconButton>
+                <AddButton buttonType={'ADD_ITEM'} buttonText={'Add'} ingredientId={ingredient.id} quantity={1} />
+              </IconButton>}>
+              <NavLink to={`/ingredients/${ingredient.id}`}>
+                <img src={ingredient.image} />
+              </NavLink>
+            </GridTile>
+          ))}
+        </GridList>
       </div>
     </div>
   )
--- client/components/main.js
+++ client/components/main.js
@@ -4,15 +4,21 @@ import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 import { logout, emptyCart } from '../store'
 import CartForNav from './CartForNav'
+import AppBar from 'material-ui/AppBar'
+import RaisedButton from 'material-ui/RaisedButton'
 /**
  * COMPONENT
  *  The Main component is our 'picture frame' - it displays the navbar and anything
  *  else common to our entire app. The 'picture' inside the frame is the space
  *  rendered out by the component's `children`.
  */
+
+const style = {
+  margin: 12,
+};
+
 const Main = (props) => {
   const { children, handleClick, isLoggedIn, userType } = props
-  console.log('philip', userType)
   return (
     <div>
       <h1>CALORIE CART</h1>
@@ -21,26 +27,48 @@ const Main = (props) => {
           isLoggedIn
             ? <div>
               {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <Link to="/ingredients">Ingredients</Link>
-              <Link to="/user">User</Link>
-              <Link to="/orderhistory">Order History</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
+              <Link to="/home">
+                <RaisedButton label="Home" primary={true} style={style} />
+              </Link>
+              <Link to="/ingredients">
+                <RaisedButton label="Ingredients" primary={true} style={style} />
+              </Link>
+              <Link to="/user">
+                <RaisedButton label="User" primary={true} style={style} />
+              </Link>
+              <Link to="/orderhistory">
+                <RaisedButton label="Order History" primary={true} style={style} />
+              </Link>
+              <a href="#" onClick={handleClick}>
+                <RaisedButton label="Logout" primary={true} style={style} />
+              </a>
               {
                 userType === 'admin' ?
                   <div>
-                    <Link to="/product-post">Post An Ingredient</Link>
-                    <Link to="/view-all-orders">View All Orders</Link>
+                    <Link to="/product-post">
+                      <RaisedButton label="Post An Ingredient" secondary={true} style={style} />
+                    </Link>
+                    <Link to="/view-all-orders">
+                      <RaisedButton label="View All Orders" secondary={true} style={style} />
+                    </Link>
                   </div>
                   : <div />
               }
             </div>
             : <div>
               {/* The navbar will show these links before you log in */}
-              <Link to="/home">Home</Link>
-              <Link to="/ingredients">Ingredients</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
+              <Link to="/home">
+                <RaisedButton label="Home" primary={true} style={style} />
+              </Link>
+              <Link to="/ingredients">
+                <RaisedButton label="Ingredients" primary={true} style={style} />
+              </Link>
+              <Link to="/login">
+                <RaisedButton label="Login" primary={true} style={style} />
+              </Link>
+              <Link to="/signup">
+                <RaisedButton label="Signup" primary={true} style={style} />
+              </Link>
             </div>
         }
         <CartForNav />
