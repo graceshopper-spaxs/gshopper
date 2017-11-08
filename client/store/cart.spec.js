f2e9d6487486a8cbfe6d3f2161b192230febc2d5
bluufish <simonmchan92@gmail.com>
2017-11-08 00:29:14 -0500

    test: refactor cartspec to have hardcoded actions

--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -20,13 +20,13 @@ describe('Cart Reducer', () => {
     describe('Add item', () => {
         
         it('adds an item to the cart', () => {
-            const action = addItem(1, 2)
+            const action = {type: "ADD_ITEM", cartItem: {ingredientId: 1, quantity: 2}}
             store.dispatch(action)
             expect(store.getState()[0].quantity).to.be.equal(2)
         })
 
         it('updates quantity if item is already in cart', () => {
-            const action = addItem(1, 2)
+            const action = {type: "ADD_ITEM", cartItem: {ingredientId: 1, quantity: 2}}
             store.dispatch(action)
             store.dispatch(action)
             expect(store.getState()[0].quantity).to.be.equal(4)
@@ -35,8 +35,8 @@ describe('Cart Reducer', () => {
 
     describe('Remove item', ()=> {
         it('removes an item from the cart', () => {
-            const addItemAction = addItem(1,2)
-            const removeItemAction = removeItem(1)
+            const addItemAction = {type: "ADD_ITEM", cartItem: {ingredientId: 1, quantity: 2}}
+            const removeItemAction = {type: "REMOVE_ITEM", ingredientId: 1}
             store.dispatch(addItemAction)
             store.dispatch(removeItemAction)
             expect(store.getState().length).to.be.equal(0)
@@ -45,8 +45,8 @@ describe('Cart Reducer', () => {
 
     describe('Update item', ()=> {
         it('Updates the quantity of an item from the cart', () => {
-            const addItemAction = addItem(1,2)
-            const updateItemAction = updateItem(1,7)
+            const addItemAction = {type: "ADD_ITEM", cartItem: {ingredientId: 1, quantity: 2}}
+            const updateItemAction = {type: "UPDATE_ITEM", cartItem: {ingredientId: 1, quantity: 7}}
             store.dispatch(addItemAction)
             expect(store.getState()[0].quantity).to.be.equal(2)
             store.dispatch(updateItemAction)
